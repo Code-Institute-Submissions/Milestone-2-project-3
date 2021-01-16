420d4c0145dd5d26d8ce281ffb08bed0374f8d05
GreigBenton <greigbenton101@gmail.com>
2021-01-16 12:07:59 +0000

    added contact form + styling via css and made responsive

--- assets/js/ani.js
+++ assets/js/ani.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     $("#review-bubble").mouseleave(function() { 
         $("#review-bubble p").slideToggle("slow", 
                 function callback() { 
-            $("#review-bubble p").text("I had just found out that my great grandparents were related to French Royalty so I decided to book a trip to my ancestral homeland (or at least that's what I'm going to tell my friends!").css("font-size", "16px");
+            $("#review-bubble p").text("'I had just found out that my great grandparents were related to French Royalty so I decided to book a trip to my ancestral homeland (or at least that's what I'm going to tell my friends!'").css("font-size", "14px");
         }); 
     }); 
 
@@ -23,7 +23,7 @@ $(document).ready(function() {
     $("#review-bubble-2").mouseleave(function() { 
         $("#review-bubble-2 p").slideToggle("slow", 
                 function callback() { 
-            $("#review-bubble-2 p").text("I've been a massive history fan since I learnt about the Battle of Hastings back in school. I managed to have a perfect holiday and learn about something new while I was there!").css("font-size", "16px");
+            $("#review-bubble-2 p").text("'I've been a massive history fan since I learnt about the Battle of Hastings back in school. I managed to have a perfect holiday and learn about something new while I was there!'").css("font-size", "14px");
         }); 
     }); 
     
